48bd16f4ec85a8b8db6ef79846c3332fc1198a35
unknown <fysalkt@gmail.com>
2020-05-20 16:45:12 +0300

    4thday2

--- js/ahc.js
+++ js/ahc.js
@@ -1,4 +1,9 @@
-AOS.init();
+AOS.init({
+  duration: 1000,
+  easing: "ease-in-out",
+  once: true,
+  mirror: false
+});
 
 const $dropdown = $(".dropdown");
 const $dropdownToggle = $(".dropdown-toggle");
